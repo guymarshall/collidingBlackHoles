34413f01d8c09d060c0d135c64f94562cd49cf15
Guy Marshall <guymarshall9@gmail.com>
2022-10-14 21:51:57 +0100

    Instantiate black hole class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         final double GRAVITATIONAL_CONSTANT = 0.000000000066743;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter mass for first black hole (kg): ");
+        double mass1 = scanner.nextDouble();
+        System.out.print("Enter mass for second black hole (kg): ");
+        double mass2 = scanner.nextDouble();
+        System.out.print("Enter distance between black holes (m): ");
+        double distance = scanner.nextDouble();
+
+        BlackHole blackHole1 = new BlackHole(mass1);
+        BlackHole blackHole2 = new BlackHole(mass2);
+
 
 //        int deltaT = 1;
 //        int time = 0;
