15fb0025b8ec044467a9ab26f5f8bb31467ac027
Guy Marshall <guymarshall9@gmail.com>
2022-10-14 21:53:17 +0100

    Remove getter and setter for BlackHole.java

--- src/BlackHole.java
+++ src/BlackHole.java
@@ -1,15 +1,5 @@
 public class BlackHole {
-    private double mass;
 
     public BlackHole(double mass) {
-        this.mass = mass;
-    }
-
-    public double getMass() {
-        return mass;
-    }
-
-    public void setMass(double mass) {
-        this.mass = mass;
     }
 }
