4d770eddb445d7b4f9f33e8f9868dac3fcff228f
Guy Marshall <guymarshall9@gmail.com>
2022-10-14 21:48:49 +0100

    Add BlackHole.java

---
+++ src/BlackHole.java
@@ -0,0 +1,15 @@
+public class BlackHole {
+    private double mass;
+
+    public BlackHole(double mass) {
+        this.mass = mass;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final double GRAVITATIONAL_CONSTANT = 0.000000000066743;
+
+//        int deltaT = 1;
+//        int time = 0;
+//        double distanceFromSun = radius;
+//        double acceleration;
+//        double velocity = 0.0;
+//        double deltaV;
+//        double deltaS;
+//
+////        ArrayList<String> results = new ArrayList<>();
+//
+//        while (true) {
+//            acceleration = -1 * (GRAVITATIONAL_CONSTANT * MASS_OF_SUN_KG) / (distanceFromSun * distanceFromSun);
+//            deltaV = acceleration * deltaT;
+//            velocity += deltaV;
+//            deltaS = velocity * deltaT;
+//            distanceFromSun += deltaS;
+//
+//            if (distanceFromSun <= 0.0) {
+//                break;
+//            }
+//
+////            results.add(String.format("Time: %ds, acceleration: %fms^-2, velocity: %fms^-1, distance: %fm", time, acceleration, velocity, distanceFromSun));
+//
+//            System.out.printf("Time: %ds, acceleration: %fms^-2, velocity: %fms^-1, distance: %fm%n", time, acceleration, velocity, distanceFromSun);
+//
+//            time += deltaT;
+//        }
     }
 }
